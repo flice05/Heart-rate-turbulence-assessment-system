d80e10b0621b10d8f9ed6dd8f9c467e33d911d7d
Nikolay <81967463+NikolayBerezhnoy@users.noreply.github.com>
2024-11-23 16:11:03 +0300

    Update sketch.ino

--- Device/sketch/sketch.ino
+++ Device/sketch/sketch.ino
@@ -1,11 +1,36 @@
+#include <GyverOLED.h>
+GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
+
+int x0 = 0;
+int y0 = 0;
+int x = 0;
+int y = 0;
+
 void setup()
 {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  oled.init();
+  oled.clear();
+  oled.print("start");
+  pinMode(8, INPUT); // Setup for leads off detection LO +
+  pinMode(9, INPUT); // Setup for leads off detection LO -
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  x0 = x;
+  x += 1;
+  y0 = y;
+  y = 63 - (analogRead(A0) * 0.0625);
+  Serial.println(analogRead(A0));
+  //oled.dot(x, y);
+  oled.line(x0, y0, x, y);
 
+  if(x>=128)
+  {
+    x = 0;
+    oled.clear();
+  }
+  
+  delay(2);
 }
